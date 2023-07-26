cb01aa6919162dc07037568fc8b1eb19b39144c8
anna125 <annamanda125@gmail.com>
2023-07-26 15:00:01 +0100

    update kasa

--- power_experiments.sh
+++ power_experiments.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #run ./power_experiments.sh $dev_auto $tag_dir
 
-#To retry the list of installed apps: adb shell "pm list packages -3"|cut -f 2 -d ":"
-#To check the coordinate to tap: settings/Developer options/Pointer location
 
 DATE=`date "+%Y%m%d_%H%M%S"`
 
-#./auto_app2multi_list_dest.sh $phone $file_dev $dir_data
-
-# The first parameter $1 should be the phone name, and a file "ids/phone_name" must exist
-# If running the script witho only the first  parameter, dev_auto is used, and all experiments executed and tagged
-# If running the script with additional parameters: $2 is the experiment file (default: 'dev_auto')
-# $3 is the tag dir. If it is 'default' the experiment is tagged in the default directory, if 'notag' the experiment is not tagged (default: 'default') 
-# $4 is the device to experiment with (if the option is not provided, all devices will be tested)
-# $5 is the experiment to be started (if the option is not provided, all experiments for the chosen device will be started)
 DEV_AUTO="$1"
 TAG_DIR="$2"
 
